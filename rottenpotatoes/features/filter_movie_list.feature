641a04dbbbf2d5c837c102180d99a9d050221352
yw3241 Wang <yw3241@columbia.edu>
2021-02-15 06:39:33 +0000

    filtering tests apassed

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -24,10 +24,24 @@ Background: movies have been added to database
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I check the following ratings: R, PG
   # enter step(s) to uncheck all other checkboxes
+  Given I uncheck the following ratings: G, PG-13, NC-17
   # enter step to "submit" the search form on the homepage
+  When I press ratings_submit
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see 'The Terminator'
+  And I should see 'When Harry Met Sally'
+  And I should see 'Amelie'
+  And I should see 'The Incredibles'
+  And I should see 'Raiders of the Lost Ark'
   # enter step(s) to ensure that other movies are not visible
+  Then I should not see 'Aladdin'
+  And I should not see 'The Help'
+  And I should not see 'Chocolat'
+  And I should not see '2001: A Space Odyssey'
+  And I should not see 'Chicken Run'
 
 Scenario: all ratings selected
-  # see assignment
+  When I check the following ratings: R, G, PG, PG-13, NC-17
+  Then I should see all of the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -29,10 +29,40 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(', ').each do |rating|
+      if uncheck
+          uncheck("ratings[#{rating}]")
+      else
+          check("ratings[#{rating}]")
+      end
+  end
+end
+
+When /I press (.*)/ do |button|
+    click_button(button)
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
+  
   fail "Unimplemented"
+end
+
+Then('I should see {string}') do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+
+Then('I should not see {string}') do |text|
+  if page.respond_to? :should
+    page.should have_no_content(text)
+  else
+    assert page.has_no_content?(text)
+  end
+end
+
+Then /I should see all of the movies/ do 
+    page.all("table#movies tr").count.should == 11
 end
