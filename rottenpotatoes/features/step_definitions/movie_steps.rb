11803328d40d1bd6a77dad51bfbb520d8676b222
yw3241 Wang <yw3241@columbia.edu>
2021-02-15 07:04:05 +0000

    sorting tests green

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -18,7 +18,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.should have_content(e1)
+  page.should have_content(e2)
+  expect(page.text).to match(/.*#{e1}.*#{e2}.*/)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
