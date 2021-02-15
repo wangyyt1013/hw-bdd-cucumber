fbe05f2c97e3d97648990c38a12914774cec23f9
yw3241 Wang <yw3241@columbia.edu>
2021-02-15 05:41:45 +0000

    background steps green

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,11 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.create!(
-        title: movie["title"],
-        rating: movie["rating"],
-        release_date: movie["release_date"]
-    )
+    Movie.create!(movie)
   end
 end
 
